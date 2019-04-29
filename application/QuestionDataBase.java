8e10c85d0c049e9a99c83f4c1d477c4553c2fe8d
Andrew Frank <ajfrank5@wisc.edu>
2019-04-29 18:17:55 -0500

    added getTopics method to QuesitonDataBase

--- application/QuestionDataBase.java
+++ application/QuestionDataBase.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.Set;
 
 public class QuestionDataBase
 {
@@ -28,5 +30,17 @@ public class QuestionDataBase
             return new ArrayList<Question>();
         }
         return questionBank.get(topic);
+    }
+
+    public ArrayList<String> getTopics()
+    {
+        Set<String> set = questionBank.keySet();
+        Iterator<String> it = set.iterator();
+        ArrayList<String> topicList = new ArrayList<>();
+        while(it.hasNext())
+        {
+            topicList.add(it.next());
+        }
+        return topicList;
     }
 }
