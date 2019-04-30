e713aa5e3624b6e8239fc08af18fa5024d6eb2a3
John Harkins <jhark1221@gmail.com>
2019-04-29 19:42:10 -0500

    question commenting

--- application/Question.java
+++ application/Question.java
@@ -4,14 +4,26 @@ import javafx.scene.image.ImageView;
 
 import java.util.ArrayList;
 
+/**
+ * Question class represents a question that is to be stored in the QuestionDataBank
+ *
+ */
 public class Question
 {
-    private String meta_data;
-    private String question;
-    private String topic;
-    private ImageView image;
-    private ArrayList<Choice> choices;
+    private String meta_data; // String variable storing if the Question has been used in the Quiz
+    private String question; // String variable storing the text of the Question to be asked
+    private String topic; // String variable storing the topic this question is considered
+    private ImageView image; // String variable holding the image filepath
+    private ArrayList<Choice> choices; // ArrayList of possible choices for this question
 
+    /**
+     * Public question constructor 
+     * @param meta_data if the Question has been used
+     * @param question the question String
+     * @param topic the topic/classification of this question
+     * @param imageFilepath the file path of this question's image
+     * @param choices the ArrayList of possible choices for this question
+     */
     public Question(String meta_data, String question, String topic, String imageFilepath, ArrayList<Choice> choices) {
         this.meta_data = meta_data;
         this.question = question;
@@ -27,6 +39,14 @@ public class Question
         this.choices = choices;
     }
 
+    /**
+     * Public question constructor 
+     * @param meta_data if the Question has been used
+     * @param question the question String
+     * @param topic the topic/classification of this question
+     * @param image 
+     * @param choices the ArrayList of possible choices for this question
+     */
     public Question(String meta_data, String question, String topic, ImageView image, ArrayList<Choice> choices) {
         this.meta_data = meta_data;
         this.question = question;
@@ -35,31 +55,55 @@ public class Question
         this.choices = choices;
     }
 
+    /**
+     * Meta-Data getter
+     * @return the meta-data for this question
+     */
     public String getMeta_data()
     {
         return meta_data;
     }
 
+    /**
+     * Question getter
+     * @return the string associated with this Question(prompt)
+     */
     public String getQuestion()
     {
         return question;
     }
 
+    /**
+     * Topic getter
+     * @return the topic associated with this question
+     */
     public String getTopic()
     {
         return topic;
     }
 
+    /**
+     * 
+     * @return 
+     */
     public ImageView getImage()
     {
         return image;
     }
 
+    /**
+     * Choices getter
+     * @return the arrayList of choices associated with this question
+     */
     public ArrayList<Choice> getChoices()
     {
         return choices;
     }
 
+    /**
+     * meta-data setter
+     * @param meta_data the string to set meta-data variable to
+     */
     public void setMeta_data(String meta_data)
     {
         this.meta_data = meta_data;
